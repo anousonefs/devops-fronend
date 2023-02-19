749baad8f054974d169764f8c38e86dab7889e54
anousonefs <aworlakoumman@gmail.com>
2023-02-19 14:13:46 +0700

    add curl

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn golang2
+          Learn react
         </a>
       </header>
     </div>
